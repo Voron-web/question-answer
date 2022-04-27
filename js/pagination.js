2c7c5aa8b9f7be545c743f48dccdb16f008262c4
Voron-web <voron.web@disroot.org>
2022-04-27 08:09:37 +0300

    add functions paginations

--- js/pagination.js
+++ js/pagination.js
@@ -1,18 +1,25 @@
-
 const questions = document.querySelectorAll(".question"),
-   itemForPage = 5,
-   questionList = document.getElementById("list");
-buttonsPaginationBlock = document.getElementById("pages")
-let maxPages = (document.querySelectorAll(".question").length - document.querySelectorAll(".question").length % itemForPage) / itemForPage + 1
-activePage = 1;
+   questionList = document.getElementById("list"),
+   buttonLeft = document.querySelector(".pagination__button-left"),
+   buttonRight = document.querySelector(".pagination__button-right"),
+   buttonsPaginationBlock = document.getElementById("pages"),
+   itemForPage = 5;
+
+
+let maxPages = (document.querySelectorAll(".question").length - document.querySelectorAll(".question").length % itemForPage) / itemForPage + 1,
+   activePage = 1,
+   buttonsPageArray = [],
+   pageButton;
+
 
 createPaginationButtons()
+console.log(buttonsPageArray);
 
 
 function fillPage(wrapper) {
    wrapper.innerHTML = "";
    let startBlock = (activePage - 1) * itemForPage;
-   // console.log(startBlock);
+
    for (i = startBlock; i < itemForPage + startBlock; i++) {
       if (i >= questions.length) {
          break;
@@ -25,15 +32,53 @@ function createPaginationButtons() {
    buttonsPaginationBlock.innerHTML = "";
 
    for (i = 1; i <= maxPages; i++) {
-      let pageButton = document.createElement('div');
-      pageButton.className = "pagination__page pagination__button button16_24";
+      pageButton = document.createElement('div');
+
+      if (i == 1) {
+         pageButton.className = "pagination__page pagination__button button16_24";
+      }
+      else {
+         pageButton.className = "pagination__page pagination__button button16_24";
+      };
       pageButton.innerHTML = i;
       buttonsPaginationBlock.append(pageButton);
-      // buttonsPaginationBlock.append("<div class="pagination__page pagination__button button16_24">"i"</div>")
+      buttonsPageArray[i - 1] = pageButton;
    }
-   var buttonsPage = document.querySelectorAll(".pagination__page");
-   buttonsPage[0].classList.add("active");
+   buttonsPageArray[0].classList.add('active');
+};
+
+function clickButton() {
+   buttonsPageArray.forEach((item) => {
+      item.classList.remove('active');
+   });
+
+   buttonsPageArray[activePage - 1].classList.add('active');
+   fillPage(questionList);
+   window.scrollTo(0, 0);
 }
 
-createPaginationButtons()
 fillPage(questionList);
+
+
+buttonRight.addEventListener('click', () => {
+   if (activePage !== maxPages) {
+      activePage++;
+      clickButton();
+   }
+});
+
+buttonLeft.addEventListener('click', () => {
+   if (activePage !== 1) {
+      activePage--;
+      clickButton();
+   }
+   console.log("left")
+});
+
+buttonsPageArray.forEach((item, index) => {
+   item.addEventListener('click', () => {
+      activePage = index + 1;
+      clickButton();
+      console.log(activePage);
+   });
+});
